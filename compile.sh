1561263115406e5ad4b5f95139369ed6efb74d4f
annie <riverknuuttila2@outlook.com>
2026-02-15 22:48:12 -0500

    copyright

--- compile.sh
+++ compile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#2048Squared (title pending) copyright (c) 2026 River Knuuttila, common alias: Annie Valentine or aval. All Rights Reserved.
+#Do not redistribute or reuse code without accrediting and explicit permission from author.
+#Contact:
+#+1 (808) 223 4780
+#riverknuuttila2@outlook.com
+
 cd "$(dirname "$0")/build" \
   && cmake ../engine -Dpybind11_DIR=$(python3 -m pybind11 --cmakedir) \
   && make -j$(nproc) \
